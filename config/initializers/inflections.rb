3edbd1a3b668f2068c63ab386ab28716eb0c17dd
estercarvalho346@gmail.com <ester.oliveira@mobile2you.com.br>
2022-01-21 19:27:07 -0300

    altered file

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,8 +1,3 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'hero', 'heroes'
 end
-
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
